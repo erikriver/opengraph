5120f749f197040a9ab725dffd2a804a6939c596
Erik Rivera <erik.river@gmail.com>
2011-06-17 17:57:48 -0500

    improving setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,18 @@ version = '0.3'
 setup(name='opengraph',
       version=version,
       description="A module to parse the Open Graph Protocol",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='opengraph protocol',
+      long_description=open("README.rst").read() + "\n",
+      classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python',
+      'Topic :: Text Processing :: Markup :: HTML',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='opengraph protocol facebook',
       author='Erik Rivera',
-      author_email='erik@rivera.com',
-      url='http://rivera.pro',
+      author_email='erik.river@gmail.com',
+      url='https://github.com/erikriver/opengraph',
       license='MIT',
       packages=find_packages(exclude=['ez_setup', 'tests']),
       include_package_data=True,
