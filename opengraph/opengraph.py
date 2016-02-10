7025765636266e8066a24ddb18c29eb85030d964
Mar Bartolome <mar@lystable.com>
2016-02-10 13:35:47 +0000

    Python3 compatibility

--- opengraph/opengraph.py
+++ opengraph/opengraph.py
@@ -1,7 +1,12 @@
 # encoding: utf-8
 
 import re
-import urllib2
+
+try:
+    import urllib2
+except ImportError:
+    from urllib import request as urllib2
+
 try:
     from bs4 import BeautifulSoup
 except ImportError:
@@ -29,12 +34,12 @@ class OpenGraph(dict):
 
         for k in kwargs.keys():
             self[k] = kwargs[k]
-        
+
         dict.__init__(self)
-                
+
         if url is not None:
             self.fetch(url)
-            
+
         if html is not None:
             self.parser(html)
 
@@ -43,14 +48,14 @@ class OpenGraph(dict):
 
     def __getattr__(self, name):
         return self[name]
-            
+
     def fetch(self, url):
         """
         """
         raw = urllib2.urlopen(url)
         html = raw.read()
         return self.parser(html)
-        
+
     def parser(self, html):
         """
         """
@@ -72,22 +77,22 @@ class OpenGraph(dict):
                         pass
 
     def valid_attr(self, attr):
-        return hasattr(self, attr) and len(self[attr]) > 0
+        return self.get(attr) and len(self[attr]) > 0
 
     def is_valid(self):
         return all([self.valid_attr(attr) for attr in self.required_attrs])
-        
+
     def to_html(self):
         if not self.is_valid():
             return u"<meta property=\"og:error\" content=\"og metadata is not valid\" />"
-            
+
         meta = u""
         for key,value in self.iteritems():
             meta += u"\n<meta property=\"og:%s\" content=\"%s\" />" %(key, value)
         meta += u"\n"
-        
+
         return meta
-        
+
     def to_json(self):
         # TODO: force unicode
         global import_json
@@ -96,9 +101,9 @@ class OpenGraph(dict):
 
         if not self.is_valid():
             return json.dumps({'error':'og metadata is not valid'})
-            
+
         return json.dumps(self)
-        
+
     def to_xml(self):
         pass
 
